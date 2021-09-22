48f4f7f1fbd6361841df67cd80bf41aa7ab5f7c4
Uma821 <uma.ohmoto@gmail.com>
2021-09-22 22:49:12 +0900

    単項プラスと単項マイマスを追加

--- 9cc.c
+++ 9cc.c
@@ -160,8 +160,10 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
+// expr = mul ("+" mul | "-" mul)*
 Node *expr() {
   Node *node = mul();
 
@@ -175,19 +177,30 @@ Node *expr() {
   }
 }
 
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
 }
 
+// unary = ("+" | "-")? primary
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
+// primary = num | "(" expr ")"
 Node *primary() {
   // 次のトークンが"("なら、"(" expr ")"のはず
   if (consume('(')) {
--- test.sh
+++ test.sh
@@ -16,8 +16,8 @@ assert() {
   fi
 }
 
-assert 47 "5+6*7"
-assert 15 "5*(9-6)"
-assert 4 "(3+5)/2"
+assert 10 "-10+20"
+assert 76 "4 +( (22/-7) +-5 )*-9"
+assert 18 "7/4*-5++23"
 
 echo OK
